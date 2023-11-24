f3dd51ccc9b8560d88faf08b8adee158f60e05c4
Brian rimi <107041935+remydev47@users.noreply.github.com>
2023-11-24 16:00:18 +0300

    refactor

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,23 +10,43 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-        body: Stack(children: [
-      ListView(
-          physics: AlwaysScrollableScrollPhysics(),
-          padding: EdgeInsets.zero,
-          scrollDirection: Axis.vertical,
-          shrinkWrap: false,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage('background.png'),
-                ),
-              ),
-            )
-          ])
-    ]));
+      body: Column(children: [
+        Container(
+          height: 200,
+          width: screenWidth,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/background.png'),
+            ),
+          ),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/profileImage.webp'),
+                          fit: BoxFit.cover,
+                        ),
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        )
+      ]),
+    );
   }
 }
